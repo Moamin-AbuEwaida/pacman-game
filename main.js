72c59198fa9250add115a4d9cae636870a651500
Moamin <moamin.salamah@gmail.com>
2022-11-19 10:21:45 +0100

    creating pacman with movement

--- main.js
+++ main.js
@@ -8,16 +8,67 @@ class Boundary {
     static width= 40;
     static height= 40;
     constructor({position}){
-        this.position = position
-        this.width = 40
-        this.height= 40
+        this.position = position;
+        this.width = 40;
+        this.height= 40;
     };
     draw(){
-        c.fillStyle = 'blue'
+        c.fillStyle = 'blue';
         c.fillRect(this.position.x, this.position.y, this.width, this.height);
     };
 };
 
+class Player {
+    constructor({position, velocity}){
+        this.position = position;
+        this.velocity = velocity;
+        this.radius = 15;
+    }
+
+    draw(){
+        c.beginPath();
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+        c.fillStyle = 'yellow';
+        c.fill();
+        c.closePath();
+    }
+
+    update(){
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+    }
+};
+
+
+const boundaries = [];
+const player = new Player({
+    position:{
+        x: Boundary.width + Boundary.width/2,
+        y: Boundary.height + Boundary.height/2
+    }, 
+    velocity:{
+        x:0,
+        y:0
+    }
+});
+
+const keys ={
+    w:{
+        pressed: false
+    },
+    a:{
+        pressed: false
+    },
+    s:{
+        pressed: false
+    },
+    d:{
+        pressed: false
+    },
+}
+
+let lastKey = '';
 const map=[
     ['-','-','-','-','-','-'],
     ['-',' ',' ',' ',' ','-'],
@@ -25,8 +76,6 @@ const map=[
     ['-',' ',' ',' ',' ','-'],
     ['-','-','-','-','-','-']
 ];
-
-const boundaries = [];
 
 map.forEach((row, i)=>{
     row.forEach((symbol, j)=>{
@@ -43,6 +92,64 @@ map.forEach((row, i)=>{
     })
 });
 
+function animate(){
+    requestAnimationFrame(animate);
+    c.clearRect(0,0,canvas.width, canvas.height);
+    boundaries.forEach(boundary=>{
+        boundary.draw()
+    });
+    player.update();
+    player.velocity.x = 0;
+    player.velocity.y = 0;
+
+    if(keys.w.pressed && lastKey === 'w'){
+        player.velocity.y = -5;
+    } else if(keys.a.pressed && lastKey === 'a'){
+        player.velocity.x = -5;
+    } else if(keys.s.pressed && lastKey === 's'){
+        player.velocity.y = 5;
+    } else if(keys.d.pressed && lastKey === 'd'){
+        player.velocity.x = 5;
+    }
+};
+
+animate()
 
 
-boundaries.forEach(boundary=>boundary.draw())
+addEventListener('keydown', ({key})=>{
+    switch (key) {
+        case 'w':
+            keys.w.pressed = true;
+            lastKey = 'w';
+            break;
+        case 's':
+            keys.s.pressed = true;   
+            lastKey = 's';         
+            break;
+        case 'd':
+            keys.d.pressed = true;
+            lastKey = 'd';            
+            break;
+        case 'a':
+            keys.a.pressed = true;
+            lastKey = 'a';
+            break;
+    }
+}); 
+
+addEventListener('keyup', ({key})=>{
+    switch (key) {
+        case 'w':
+            keys.w.pressed = false;
+            break;
+        case 's':
+            keys.s.pressed = false;
+            break;
+        case 'd':
+            keys.d.pressed = false;
+            break;
+        case 'a':
+            keys.a.pressed = false;
+            break;
+    }
+});
